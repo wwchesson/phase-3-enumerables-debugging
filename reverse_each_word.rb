f82804ae21e22bd496e433aa260f5b30d2318630
Will <willchesson@gmail.com>
2022-05-11 11:59:19 -0400

    commit tree 41a55acba7642956820d65fd59090a34603708c5

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = []
+
+    words.each do |word|
+      reversed_words << word.reverse
+    end 
+
+    reversed_words.join(" ")
+    #all of this can refactor to
+    #sentence.split.map(&:reverse).join(" ")
+    #map returns an array, (&:reverse) will reverse all words in that array 
+end
+
+binding.pry 
+0
+#have to add the zero at the end bc by default cannot use a breakpoint
+#as last line of code
